1d11f0d67e23cfa59504422ba89a5dcc2e8c56d8
Vinicius Rodrigues <vinicius.1986@gmail.com>
2020-02-11 08:40:30 -0300

    Adivcionado models com talebas de pessoas e atividades e criado de teste para manipular os dados do banco de dados

--- models.py
+++ models.py
@@ -12,6 +12,7 @@ Base.query = db_session.query_property()
 
 class Pessoas(Base):
     __tablename__='pessoas'
+    #O parâmetro primarykey cria altomaticamente um numero para cada registro novo, tornando unico
     id = Column(Integer, primary_key=True)
     nome = Column(String(40), index=True)
     idade = Column(Integer)
@@ -19,12 +20,35 @@ class Pessoas(Base):
     def __repr__(self):
         return f'<Pessoa {self.nome}>'
 
+    # O comando commit() finaliza todos os comando que serão executados no banco de dados
+    # O comando add adiciona o registro ao banco
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
+
+    def delete(self):
+        db_session.delete(self)
+        db_session.commit()
+
 class Atividades(Base):
     __tablename__='atividades'
     id = Column(Integer, primary_key=True)
     nome = Column(String(80))
     pessoa_id = Column(Integer, ForeignKey('pessoas.id'))
     pessoa = relationship("Pessoas")
+
+    def __repr__(self):
+        return f'<Atividades {self.nome}>'
+
+    # O comando commit() finaliza todos os comando que serão executados no banco de dados
+    # O comando add adiciona o registro ao banco
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
+
+    def delete(self):
+        db_session.delete(self)
+        db_session.commit()
 
 def init_db():
     Base.metadata.create_all(bind=engine)
--- utils.py
+++ utils.py
@@ -1,24 +1,52 @@
-from models import Pessoas, db_session
+from models import Pessoas
 
 def insere_pessoas():
-    pessoa = Pessoas(nome='Rafael', idade=29)
+    pessoa = Pessoas(nome='vinicius', idade=29)
     print(pessoa)
-    db_session.add(pessoa)
-    db_session.commit()
 
-def consulta():
+    pessoa.save()
+
+def consulta_pessoas():
+    # O metodo query.all() busca todos os registros depois insere eles em uma lista.
+    #OBS: Se a quantidade de registros for muito grande, em algum momento isso se torna um problema
     pess = Pessoas.query.all()
-    pessoa = Pessoas.query.filter_by(nome='Rafael').first()
-    print(pessoa)
+    print(pess[0].nome,pess[0].idade)
+    print(pess)
+
+    # Aqui o campo nome já foi indexado na criação no arquivo modelo, isso facilita e reduz gargalos a longo prazo
+    # o banco tráz só o necessário para a consulta.
+    # No caso ele tráz uma lista que só pdoe ser acessada utilizando um for
+    # utilizando o first() ele atribui a pessoa só 1 objeto enão uma lista.
+    pessoa = Pessoas.query.filter_by(nome='vinicius').first()
     print(pessoa.nome)
     print(pessoa.idade)
-    for p in pess:
-        print(p)
-    print(f'Quantidade de pessoas é {len(pess)}')
+    print(f'Total de registros {len(pess)}')
+
+def altera_pessoa():
+    pessoa = Pessoas.query.filter_by(nome='vinicius').first()
+    pessoa.idade = 21
+    pessoa.save()
+
+
+def exclui_pessoa():
+    try:
+        pessoa = Pessoas.query.filter_by(nome='Rafael').first()
+        pessoa.delete()
+    except:
+        if pessoa == None:
+            print('Não retornou nada')
+        else:
+            print('erro desconhecido')
 
 
 
 
+
+
+# Este comando verifica se o arquivo esta sendo executado nele mesmo e não chamado em outro lugar
+# e executa o comando consulta() ou insere_pessoas()
 if __name__ == '__main__':
-    insere_pessoas()
-    consulta()
+    #insere_pessoas()
+    #altera_pessoa()
+    exclui_pessoa()
+    consulta_pessoas()
